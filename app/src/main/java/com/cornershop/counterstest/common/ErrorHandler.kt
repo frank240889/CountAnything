398696533e08ec3f7d16870e668849e90a617bda
Franco Omar Castillo Bello <francocastillo2@hotmail.com>
2021-08-30 09:50:11 -0500

    - Added excetion type.

--- app/src/main/java/com/cornershop/counterstest/common/ErrorHandler.kt
+++ app/src/main/java/com/cornershop/counterstest/common/ErrorHandler.kt
@@ -6,6 +6,7 @@ import com.cornershop.counterstest.exceptions.NotItemsSelectedForDeletion
 import com.cornershop.counterstest.interfaces.ExceptionHandler
 import com.cornershop.counterstest.interfaces.ResourceManager
 import retrofit2.HttpException
+import java.io.InterruptedIOException
 import java.net.ConnectException
 import java.net.SocketException
 import javax.inject.Inject
@@ -20,7 +21,8 @@ class ErrorHandler @Inject constructor(
     override fun handleError(t: Throwable) = when (t) {
         is HttpException,
         is ConnectException,
-        is SocketException -> resourceManager.getString(R.string.connection_error_description)
+        is SocketException,
+        is InterruptedIOException -> resourceManager.getString(R.string.connection_error_description)
         is InvalidTitleCounterException -> resourceManager.getString(R.string.invalid_title_counter)
         is NotItemsSelectedForDeletion -> resourceManager.getString(R.string.no_items_to_delete)
         else -> resourceManager.getString(R.string.generic_error_description)
